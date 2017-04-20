6065a398432333ee73824f80615875ef6a9a3b55
logan <logan@swango.xyz>
2017-04-20 08:08:11 -0400

    cleaned up config

--- config.js
+++ config.js
@@ -8,25 +8,36 @@
 
  const serverName = 'Server Running SLmod';
 
-// these variables should be given to you by the person hosting the stats server
+
+
+// these 2 variables should be given to you by the person hosting the stats server
 
  const serverId = 0;
  const serverToken = "token_for_server_0";
 
-// Default: http://1stcav.servegame.com:229/api/dcs/slmod/update
-// Hosted on my local machine and routed using ddns
 
-const sendURL = 'http://1stcav.servegame.com:229/api/dcs/slmod/update';
+
+// Default: [ S3 Server URL/IP ]/api/dcs/slmod/update
+// The link to your S3 server + and the default S3 API route
+
+const s3Server = 'http://1stcav.servegame.com:229';
+  const sendURL = s3Server + '/api/dcs/slmod/update';
+
+
 
 // Default: 'C:\\Users\\dcs\\Saved Games\\Slmod\\slmod_official_lua_stats.lua';
 // The Directory + Filename of the lua stats file for node
 
 const statsDir = 'C:\\Users\\dcs\\Saved Games\\Slmod\\slmod_official_lua_stats.lua';
 
+
+
 // Default: 'stats'
 // The Name of the Table in the lua file to parse into JSON
 
 const statsVar = 'stats';
+
+
 
 // Default: '* * * * *'
 // Default: (AKA, once every minute)
@@ -35,10 +46,14 @@ const statsVar = 'stats';
 
 const schedule = '* * * * *';
 
+
+
 // Default: false
 // Make true if you want to Backup the latest sent JSON data
 
 const writeJson = false;
+
+
 
 // Default: process.cwd() + '\\json\\stats.json'
 // Or specify a specific Directory + Filename to Backup the latest sent JSON data to
