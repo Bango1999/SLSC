36ee3c402990306eec87d107a76b65abab688e7f
_bango_ <logan@swango.xyz>
2017-05-09 12:23:05 -0400

    better config. had forgotten dcs version!

--- config.js
+++ config.js
@@ -3,66 +3,73 @@
  /  Edit this file according to your needs   /
  /------------------------------------------*/
 
+
+
+ // ---------------------------
 // Default: 'Server Running SLmod'
 // what is the server called?
-
+// ---------------------------
  const serverName = 'Server Running SLmod';
 
-
+// ---------------------------
 // Default: 0, 'token_for_server_0'
 // these 2 variables should be given to you by the person hosting the stats server
 // Only necessary to configure if you are POSTing to an S3 server
-
+// ---------------------------
  const serverId = 0;
  const serverToken = 'token_for_server_0';
 
 
-
+// ---------------------------
 // Default: http://localhost:4000/api/dcs/slmod/update
 // The link to your S3 server + and the default S3 API route
 //   If you are NOT running an S3 server, you will need to edit the route in postPath to fit the route for your own API
 //   If you ARE running an S3 server, only edit serverURL to match where you are hosting your S3 server
+// ---------------------------
 const serverURL = 'http://localhost:4000';
   const postPath = serverURL + '/api/dcs/slmod/update';
 
 
-
-// Default: 'C:\\Users\\dcs\\Saved Games\\Slmod\\slmod_official_lua_stats.lua';
+// ---------------------------
+// Default: 'C:\\Users\\dcs\\Saved Games\\DCS\\Slmod\\slmod_official_lua_stats.lua';
 // The Directory + Filename of the lua stats file for node
-// You will likely wish to edit windowsUsername, while leaving the rest as-is
+//   You will likely wish to edit windowsUsername
+//   If you are running a DCS other than stable, edit DCS folder name in dcsVersion
+//----------------------------
 const windowsUsername = 'dcs';
-  const statsDir = 'C:\\Users\\'+ windowsUsername +'\\Saved Games\\Slmod\\slmod_official_lua_stats.lua';
+const dcsVersion = 'DCS';
+  const statsDir = 'C:\\Users\\'+ windowsUsername +'\\Saved Games\\'+ dcsVersion +'\\Slmod\\slmod_official_lua_stats.lua';
 
 
-
+// ---------------------------
 // Default: '* * * * *'
 // Default: (AKA, once every minute)
 // How often should it check for new stats?
-// https://code.tutsplus.com/tutorials/scheduling-tasks-with-cron-jobs--net-8800
-
+//   https://code.tutsplus.com/tutorials/scheduling-tasks-with-cron-jobs--net-8800
+// ---------------------------
 const schedule = '* * * * *';
 
 
-
+// ---------------------------
 // Default: false
 // Make true if you want to Backup the latest sent JSON data
-
+// ---------------------------
 const writeJson = false;
 
 
-
+// ---------------------------
 // Default: process.cwd() + '\\json\\stats.json'
 // Or specify a specific Directory + Filename to Backup the latest sent JSON data to
-
+// ---------------------------
 const jsonDir = process.cwd() + '\\json\\stats.json';
 
 
-
+// ---------------------------
 // Default: 'stats'
 // The Name of the Table in the lua file to parse into JSON
 // This is the default value for all of SLmod, so best leave it alone
 //  unless you know something I don't
-
+// ---------------------------
 const statsVar = 'stats';
 
 
