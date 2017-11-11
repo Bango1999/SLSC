e3f08c5ff2226f09c13157b2ebcb3a936b1b7cb5
WORKHORSE\bango <logan@swango.xyz>
2017-11-11 01:41:44 -0500

    final version of updater?

--- Update_SLSC.bat
+++ Update_SLSC.bat
@@ -3,13 +3,13 @@ for /f %%i in ('call git status --porcelain') do set stash=%%i
 if not [%stash%] == [] (
   echo Stashing local changes...
   call git add .
-  call git stash
+  call git stash -q
 )
 echo Updating SLSC App...
 call git pull origin master
 if not [%stash%] == [] (
   echo Restoring local changes...
-  call git stash apply
+  call git stash apply -q
 )
 echo Updating SLSC Dependencies...
 call npm update
