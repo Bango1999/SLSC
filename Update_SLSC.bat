792846637325504873898c05a57961e3bfc79d51
WORKHORSE\bango <logan@swango.xyz>
2017-11-11 01:24:58 -0500

    random commit

--- Update_SLSC.bat
+++ Update_SLSC.bat
@@ -1,19 +1,12 @@
 @echo off
 
-rem if the hash doesnt exist, we dont know whether to stash or not which is a problem
-  If Not Exist hash\updater.hashfile (
-    echo ERROR: missing file 'hash\updater.hashfile'. Updater will now close.
-    pause
-    Exit /b
-  )
 
-rem hash the current config.js into a variable md5_var
-  call hash\MD5.bat config.js md5_var
-rem pipe hash to file
-  echo %md5_var% > hash\updater.hashfile.temp
+for /f %%i in ('call git status --porcelain') do set stash=%%i
+echo stash = %stash%
+pause
+exit /b
 
-rem compare the old hashfile with the current hashfile
-  call FC hash\updater.hashfile hash\updater.hashfile.temp >NUL && set stash_var=0 || set stash_var=1
+
 
 rem delete the temp (current) hashfile
   del /f hash\updater.hashfile.temp
